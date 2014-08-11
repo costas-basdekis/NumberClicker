e4886d02ebced1dda7bfb08106a9b43cb4e45aee
Costas Basdekis <costas.basdekis@gmail.com>
2014-08-12 00:32:55 +0100

    Add some more buyables

--- js/src/buyables.js
+++ js/src/buyables.js
@@ -15,8 +15,7 @@ var buyables = {
 		renameTo: 'Integers',
 		rateMultiply: 2,
 		enabled: function(){
-			return !this.buyables.integers.bought() &&
-				   this.buyables.naturals.count() >= 2;
+			return this.buyables.naturals.count() >= 2;
 		},}),
 	isomorphisms: new ImproveResearchModel({
 		name: 'Isomorphisms',
@@ -26,8 +25,67 @@ var buyables = {
 		target: 'naturals',
 		rateMultiply: 1.5,
 		enabled: function(){
-			return !this.buyables.isomorphisms.bought() &&
-				   this.buyables.naturals.count() >= 5 &&
+			return this.buyables.naturals.count() >= 5 &&
 				   this.buyables.integers.bought();
+		},}),
+	decimals: new BuildingModel({
+		name: 'Decimals',
+		cost: 4,
+		rate: 1,
+		enabled: function() {
+			return this.buyables.naturals.count() >= 10;
+		},}),
+	repeating: new ImproveResearchModel({
+		name: 'Repeating decimals',
+		caption: 'They don\'t end, but repeat, but repeat, but repeat...',
+		description: '+1Hz for Decimals',
+		cost: 10,
+		target: 'decimals',
+		renameTo: 'Repeating decimals',
+		rateMultiply: 2,
+		enabled: function(){
+			return this.buyables.decimals.count() >= 2;
+		},}),
+	rationals: new ImproveResearchModel({
+		name: 'Rationals',
+		caption: 'Come to your senses, the denominator can be any integer',
+		description: '+1Hz for Repeating decimals',
+		cost: 15,
+		target: 'decimals',
+		renameTo: 'Rationals',
+		rateMultiply: 1.5,
+		enabled: function(){
+			return this.buyables.decimals.count() >= 5 &&
+				   this.buyables.repeating.bought();
+		},}),
+	algebraics: new BuildingModel({
+		name: 'Algebraics',
+		cost: 10,
+		rate: 3.5,
+		enabled: function() {
+			return this.buyables.decimals.count() >= 10;
+		},}),
+	reals: new ImproveResearchModel({
+		name: 'Reals',
+		caption: 'Is this just fantasy?',
+		description: '+3.5Hz for algebraics',
+		cost: 25,
+		target: 'algebraics',
+		renameTo: 'Reals',
+		rateMultiply: 2,
+		enabled: function(){
+			return this.buyables.algebraics.count() >= 2;
+		},}),
+	imaginaries: new ImproveResearchModel({
+		name: 'Imaginaries',
+		caption: 'Root of a negative? What a country...',
+		description: '+3.5Hz for Reals',
+		cost: 35,
+		target: 'algebraics',
+		renameTo: 'Imaginaries',
+		rateMultiply: 1.5,
+		enabled: function(){
+			return this.buyables.algebraics.count() >= 5 &&
+				   this.buyables.reals.bought();
 		},}),
 };
